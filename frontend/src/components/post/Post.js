b49126eb49bf9303e8e24a90ed560544a5626a1d
David Brito <39559632+davbrito@users.noreply.github.com>
2020-08-27 13:17:30 -0400

    se extrajeron los componentes PostImage y PostDescription

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,42 +1,53 @@
-import React,{Component} from 'react'
-import {Button} from 'react-materialize'
+import React, { Component } from 'react';
+import { Button } from 'react-materialize';
 
 
-class Post extends Component{
+function PostImage(props) {
+    return (
+        <div className="card-image">
+            <img src={props.url} alt="" />
+        </div>
+    )
+}
+function PostDescription(props) {
+    return (
+        <div className="card-content">
+            <p>{props.text}</p>
+        </div>
+    )
+}
 
-    constructor(props){
+class Post extends Component {
+
+    constructor(props) {
         super(props)
-        this.state = {love: this.props.love}
+        this.state = { love: this.props.love }
 
         this.handleLove = this.handleLove.bind(this);
     }
 
-    handleLove(e){
-       this.setState(state =>({love: !this.state.love}))
-       e.preventDefault();
+    handleLove(e) {
+        this.setState(state => ({ love: !state.love }))
+        e.preventDefault();
     }
 
-    render(){
-        return(
-              <div className="card">
-                <div className="card-image">
-                  <img src={this.props.image} alt="" />
-                </div>
-                <div className="card-content">
-                    <p>{this.props.description}</p>
-                </div>
+    render() {
+        return (
+            <div className="card">
+                <PostImage url={this.props.image} />
+                <PostDescription text={this.props.description} />
                 <div className="card-action">
-                  <Button className="red darken-1"style={{marginRight: "5px"}} onClick={this.handleLove} >
-                    <i className="material-icons">{ this.state.love ? "favorite" : "favorite_border"}</i>
-                  </Button>
-                  <Button className="light-blue darken-3" style={{marginRight: "5px"}} >
-                    <i className="material-icons">comment</i>
-                  </Button>
-                  <Button className="gren accent-3" >
-                    <i className="material-icons">more_vert</i>
-                  </Button>
+                    <Button className="red darken-1" style={{ marginRight: "5px" }} onClick={this.handleLove} >
+                        <i className="material-icons">{this.state.love ? "favorite" : "favorite_border"}</i>
+                    </Button>
+                    <Button className="light-blue darken-3" style={{ marginRight: "5px" }} >
+                        <i className="material-icons">comment</i>
+                    </Button>
+                    <Button className="gren accent-3" >
+                        <i className="material-icons">more_vert</i>
+                    </Button>
                 </div>
-              </div>
+            </div>
         )
     }
 }
