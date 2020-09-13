06946b8424dfd89f84bb5556bc19e7af0accbddd
David Brito <39559632+davbrito@users.noreply.github.com>
2020-09-13 14:55:08 -0400

    fix

--- backend/profiles/tests.py
+++ backend/profiles/tests.py
@@ -7,17 +7,22 @@ from django.test import TestCase
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
+from profiles.models import Profile
+
+UserModel = get_user_model()
+
 
 class ProfileModelTest(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
         # Set up data for the whole TestCase
-        cls.users = [
-            User.objects.create_user(f'tester{i}', f'tester{i}@gmail.com',
-                                     f'testpasswd{i}') for i in range(5)
-        ]
-        cls.profiles = [u.profile for u in cls.users]
+        for i in range(5):
+            User.objects.create_user(
+                username=f'tester{i}',
+                email=f'tester{i}@gmail.com',
+                password=f'testpasswd{i}',
+            )
 
     def assertUserHasProfile(self, user):
         try:
@@ -27,15 +32,21 @@ class ProfileModelTest(APITestCase):
 
     def test_auto_create_on_user_create(self):
         """Test if a profile is created when a user is created."""
-        # User = get_user_model()
-        for i, user in enumerate(self.users):
+        for i, user in enumerate(UserModel.objects.all()):
             with self.subTest(i=i):
                 self.assertUserHasProfile(user)
 
+    def get_2_random_profiles(self):
+        while True:
+            a, b = random.choices(Profile.objects.all(), k=2)
+            if a != b:
+                break
+        return a, b
+
     def test_follow_unfollow(self):
         """Test if a profile is created when a user is created."""
-        # User = get_user_model()
-        a, b = random.sample(self.profiles, 2)
+        a, b = self.get_2_random_profiles()
+
         a.follow(b)
         self.assertTrue(a.follows(b))
         a.unfollow(b)
